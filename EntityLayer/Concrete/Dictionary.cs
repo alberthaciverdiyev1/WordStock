c36c9ac83b43d4c563f3b65a7004d622d667a109
alberthaciverdiyev1 <alberthaciverdiyev55@gmail.com>
2024-12-15 02:03:15 -0800

    started project

---
+++ EntityLayer/Concrete/Dictionary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Dictionary
+    {
+        public int Id { get; set; }
+        public string EngWord { get; set; }
+        public string AzeWord { get; set; }
+        public string AlternativeAzeWords { get; set; }
+        public string AlternativeEngWords { get; set; }
+    }
+}
